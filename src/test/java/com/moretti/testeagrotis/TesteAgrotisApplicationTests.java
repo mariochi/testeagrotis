db39615a8f1bfbcf1ee63b52a38f53b59c4ba53f
José Cláudio Moretti Junior <jc.moretti.jr1991@gmail.com>
2022-02-15 22:58:30 -0300

    Commit Inicial

---
+++ src/test/java/com/moretti/testeagrotis/TesteAgrotisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moretti.testeagrotis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteAgrotisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
